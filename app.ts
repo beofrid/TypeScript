744a263ffdf07382a19872fecfd1cce1995ce98d
beofrid <beofrid@gmail.com>
2022-07-25 18:05:12 -0300

    Desafio 1

--- app.ts
+++ app.ts
@@ -1,27 +1,7 @@
-let button = document.getElementById("button") 
-let input1 = document.getElementById("input1") as HTMLInputElement;
-let input2 = document.getElementById("input2") as HTMLInputElement;
+let employee = {
+    code: Number;
+    name: 'string';
+};
 
-function adicionarNumero(n1:number, n2:number){
-    return n1+n2;
-}
-
-if (button){
-    button.addEventListener('click', () =>  {
-        alert("pelo menos isso")
-        console.log(adicionarNumero(Number(input1.value), Number(input2.value)))
-    })
-
-}
-
-const animal = {
-    especie: 'pomba',
-    cor: 'branca',
-    idade: 1
-
-}
-
-const pomba: {nome: String, peso: Number} = {
-    nome: "gira",
-    peso: 2 
-}
+employee.code = 10;
+employee.name = "John";
