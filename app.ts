32af6e67248b2757396c723b4dc0561dae8a8644
beofrid <beofrid@gmail.com>
2022-07-25 18:41:34 -0300

    Desafio 2

--- app.ts
+++ app.ts
@@ -1,6 +1,42 @@
+//========DESAFIO 1=============
 let employee = {
     code: 10,
-    name: '"John"'
+    name: 'John'
 };
 
 console.log(employee.code, employee.name)
+
+
+/// nenhum erro 
+
+//========DESAFIO 2=============
+
+//========DESAFIO 2=============
+interface person {
+    nome: String, idade: Number, profissao: string
+}
+
+
+let pessoa1: person = {
+    nome: "maria",
+    idade: 29,
+profissao: "atriz"
+}
+
+let pessoa2: person =  {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro"
+}
+
+let pessoa3: person =  {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+}
+
+let pessoa4 = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
