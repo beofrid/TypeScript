9d3a475e9093c7edfecc619e79059b0c3edff9c7
beofrid <beofrid@gmail.com>
2022-07-25 18:42:07 -0300

    updates

---
+++ app.js
@@ -0,0 +1,27 @@
+//========DESAFIO 1=============
+var employee = {
+    code: 10,
+    name: 'John'
+};
+console.log(employee.code, employee.name);
+var pessoa1 = person, _a = void 0;
+pessoa1.nome;
+"maria",
+    idade;
+29,
+    profissao;
+"atriz";
+var pessoa2 = {};
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+var pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+var pessoa4 = {
+    nome: nome,
+    idade: idade,
+    profissao: profissao
+};
